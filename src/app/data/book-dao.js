cd9a231f88d3c5e021c3f9f7c4b9afea5a1c04b8
Everton Brzozowy Alves <everton.brzozowy.alves@gmail.com>
2020-08-09 17:56:04 -0300

    chore: updated bookDao

--- src/app/data/book-dao.js
+++ src/app/data/book-dao.js
@@ -36,6 +36,62 @@ class BookDao {
         }
     })
   }
+
+  findById(id) {
+    return new Promise((resolve, reject) => {
+      this._db.get(`
+        SELECT * FROM livros WHERE id = ?
+      `,
+        [id]
+      ),
+        (err, book) => {
+          if (err)
+            return reject(`It was not possible find the book.`)
+          return resolve(book)
+        }
+    })
+  }
+
+  update(book) {
+    return new Promise((resolve, reject) => {
+      this._db.run(`
+        UPDATE livros
+        SET titulo = ?, preco = ?, descricao = ?
+        WHERE id = ?
+      `,
+        [
+          book.titulo,
+          book.preco,
+          book.descricao,
+          book.id
+        ]),
+        (err) => {
+          if (err) {
+            console.error(err)
+            return reject(`It was not possible to add the book.`)
+          }
+          resolve()
+        }
+    })
+  }
+
+  remove(id) {
+    return new Promise((resolve, reject) => {
+      this._db.run(`
+        DELETE FROM livros
+        WHERE id = ?
+      `,
+        [id]
+      ),
+        (err) => {
+          if (err) {
+            console.error(err)
+            return reject(`It was not possible to delete the books`)
+          }
+          resolve()
+        }
+    })
+  }
 }
 
 module.exports = BookDao
